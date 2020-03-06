02f7e627cc63515594df942b7e3753a2b491e90e
taichanne30 <ne300209@senshu-u.jp>
2020-03-07 03:27:16 +0900

    initial commit

---
+++ kit/app/stopwatch/stopwatch.js
@@ -0,0 +1,54 @@
+((_pid, _app) => {
+
+    const zeroPad = (obj, len) => String(obj).padStart(len, '0');
+
+    const formatTime = timediff => {
+        const ordinal_sec = Math.floor(timediff / 1000);
+        const hours = Math.floor(ordinal_sec / 3600);
+        const minutes = Math.floor((ordinal_sec - (hours * 3600)) / 60);
+        const seconds = ordinal_sec - hours * 3600 - minutes * 60;
+        const millsec = timediff - ordinal_sec * 1000;
+        return `${zeroPad(hours, 2)}:${zeroPad(minutes, 2)}:${zeroPad(seconds, 2)}.${zeroPad(millsec, 3)}`
+    }
+
+    const events = {
+        init() {
+            _app.data('time', 0);
+            _app.data('accumulated_time', 0);
+            _app.data('show_text', '00:00:00.000');
+            _app.dom('.start').show();
+            _app.dom('.stop').hide();
+            _app.dom('.reset').hide();
+        },
+        start() {
+            // もしintervalが空なら
+            if(!_app.data('interval')){
+                _app.data('start', Date.now());
+                _app.dom('.start').hide();
+                _app.dom('.stop').show();
+                _app.dom('.reset').show();
+                _app.data('interval', setInterval(()=>{ _app.event('update') }, 10));
+            }
+        },
+        update() {
+            _app.data('time', _app.data('accumulated_time') + (Date.now() - _app.data('start')));
+            _app.data('show_text', formatTime(_app.data('time')));
+        },
+        stop() {
+            if(_app.data('interval')) clearInterval(_app.data('interval'));
+            _app.data('accumulated_time', _app.data('accumulated_time') + _app.data('time'));
+            _app.data('interval', null);
+            _app.dom('.start').show();
+            _app.dom('.stop').hide();
+        },
+        reset() {
+            _app.event('stop');
+            _app.event('init');
+        }
+    }
+
+    Object.keys(events).forEach((key) => _app.event(key, events[key]));
+
+    _app.event('init')();
+
+})(pid, app);
