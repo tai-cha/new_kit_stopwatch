c2515b4a0c565f010f5b8019245f343753e260ed
taichanne30 <ne300209@senshu-u.jp>
2020-03-07 16:57:51 +0900

    Add lap function

--- kit/app/stopwatch/stopwatch.js
+++ kit/app/stopwatch/stopwatch.js
@@ -1,5 +1,18 @@
 ((_pid, _app) => {
 
+    const isDisabled = selector => (_app.dom(selector).filter('.-disabled').length > 0);
+
+    const toggleDisabled = selector => {
+        const elems = _app.dom(selector);
+        Array.prototype.forEach.call(dom, (el)=>{
+            if (el.filter(':not(-disabled)').length > 0){
+                el.removeClass('-disabled');
+            } else {
+                el.addClass('-disabled');
+            }
+        });
+    }
+
     const zeroPad = (obj, len) => String(obj).padStart(len, '0');
 
     const formatTime = timediff => {
@@ -12,15 +25,21 @@
     }
 
     const currentTime = () => _app.data('accumulated_time') + _app.data('time');
+    const lapTime = () => currentTime() - (_app.data().last_lap_time || 0);
 
     const events = {
         init() {
+            _app.data('start', null);
             _app.data('time', 0);
             _app.data('accumulated_time', 0);
+            _app.data('last_lap_time', null);
             _app.data('show_text', '00:00:00.000');
+            _app.data('lap_text', null);
             _app.dom('.start').show();
             _app.dom('.stop').hide();
             _app.dom('.reset').hide();
+            _app.dom('.laps').html('');
+            _app.dom('.lap:not(.-disabled)').addClass('-disabled');
         },
         start() {
             // もしintervalが空なら
@@ -28,6 +47,7 @@
                 _app.data('start', Date.now());
                 _app.dom('.start').hide();
                 _app.dom('.stop').show();
+                _app.dom('.lap.-disabled').removeClass('-disabled');
                 _app.dom('.reset').show();
                 _app.data('interval', setInterval(()=>{ _app.event('update') }, 10));
             }
@@ -35,10 +55,14 @@
         update() {
             _app.data('time', Date.now() - _app.data('start'));
             _app.data('show_text', formatTime(currentTime()));
+            _app.data('lap_text', formatTime(lapTime()));
         },
         lap() {
-            _app.dom('ol.laps').prepend(`<li>${formatTime(currentTime())}</li>`);
-            return 
+            if(!isDisabled('.lap')){
+                _app.dom('ol.laps').prepend(`<li>${formatTime(lapTime())}</li>`);
+                _app.data('last_lap_time', currentTime());
+                return currentTime();
+            }else return false;
         },
         stop() {
             if(_app.data('interval')) clearInterval(_app.data('interval'));
@@ -46,6 +70,7 @@
             _app.data('interval', null);
             _app.dom('.start').show();
             _app.dom('.stop').hide();
+            _app.dom('.lap:not(.-disabled)').addClass('-disabled');
         },
         reset() {
             _app.event('stop');
